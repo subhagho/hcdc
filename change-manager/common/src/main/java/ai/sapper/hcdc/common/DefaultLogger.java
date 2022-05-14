0d21c96b03eb09e326fd3dcea8a8a5c87bb37740
Subho Ghosh <subho.ghosh@outlook.com>
2022-05-14 23:35:32 +0530

    Common setup

---
+++ change-manager/common/src/main/java/ai/sapper/hcdc/common/DefaultLogger.java
@@ -0,0 +1,12 @@
+package ai.sapper.hcdc.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class DefaultLogger {
+    private static Logger __default = LoggerFactory.getLogger("HCDC");
+
+    public static void error(Logger logger, Class<?> type, String message) {
+
+    }
+}
